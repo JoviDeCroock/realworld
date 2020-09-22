b1f41835456c1bbfed51855f564d5d40586e915b
Jovi De Croock <decroockjovi@gmail.com>
2020-09-23 00:46:13 +0200

    remove normalization

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,9 @@
 import { createElement, render } from 'preact';
-import { setPragma } from 'goober';
-import normalize from './global/normalizeStyles';
 import App from './App';
 
 import './assets/ionicons.woff'
 import './assets/fonts.css'
 import './assets/main.css'
 import './assets/ionicons.min.css'
-
-setPragma(createElement);
-normalize();
 
 render(<App />, document.getElementById('root'));
