0e209f4769d5441cd2d8a553e4bfd8c0c6a82b2d
Jovi De Croock <decroockjovi@gmail.com>
2020-09-24 17:52:09 +0200

    use compat

--- src/lib/progressive-hydration.js
+++ src/lib/progressive-hydration.js
@@ -1,18 +1,20 @@
 import { h } from 'preact';
 
-export function lazy(load) {
-  let component, promise;
-  return function Lazy(props) {
-    if (!promise) promise = load().then(m => component = m.default || m);
-    if (!this.waiting) this.waiting = promise.then(c => this.setState({c}));
-    if (!component) throw promise;
-    return h(component, props);
-  }
-}
+// export function lazy(load) {
+//   let component, promise;
+//   return function Lazy(props) {
+//     if (!promise) promise = load().then(m => component = m.default || m);
+//     if (!this.waiting) this.waiting = promise.then(c => this.setState({c}));
+//     if (!component) throw promise;
+//     return h(component, props);
+//   }
+// }
 
-export function Suspense(props) { return props.children; }
-Suspense.prototype.componentDidCatch = function (err) {
-  if (err && err.then) this.__d = true;
-};
+// export function Suspense(props) { return props.children; }
+// Suspense.prototype.componentDidCatch = function(e) {
+//   if (e && e.then) {
+//     this.__d = true;
+//   }
+// }
 
-// export { Suspense, lazy } from 'preact';
+export { Suspense, lazy } from 'preact';
